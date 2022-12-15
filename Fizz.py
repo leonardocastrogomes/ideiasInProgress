ab1467e76755b9a3d73e3de320024b40aabb70a8
leonardocastrogomes <119269563+leonardocastrogomes@users.noreply.github.com>
2022-12-15 15:39:43 -0300

    Update and rename 2-fizzBuzz.py to Fizz.py

--- Fizz.py
+++ Fizz.py
@@ -1,3 +1,5 @@
+# Script que recebe um número inserido pelo usuário e imprime 'Fizz' para múltiplos de 3 ou o próprio número em caso contrário
+
 numeroInt = int(input('ínsira um número inteiro:'))
 div3 = numeroInt%3
 if div3 == 0:  
