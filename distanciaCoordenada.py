51c7b8b5dae14211a7d0432e66083c9ef2e94e47
leonardocastrogomes <119269563+leonardocastrogomes@users.noreply.github.com>
2022-12-15 15:27:38 -0300

    Update and rename 1A-distanciaCoordenada.py to distanciaCoordenada.py

--- distanciaCoordenada.py
+++ distanciaCoordenada.py
@@ -1,3 +1,4 @@
+# script que recebe um par de coordenadas no eixo cartesiano e informa 'longe' ou 'perto' usando a medida 10 como referência
 import math
 x1 = int(input('insira a coordenada x1:'))
 y1 = int(input('insira a coordenada y1:'))
