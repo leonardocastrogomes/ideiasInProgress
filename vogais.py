b12d37c085720e7a27e94ade129393521b88f7d7
leonardocastrogomes <119269563+leonardocastrogomes@users.noreply.github.com>
2022-12-15 16:04:49 -0300

    Update and rename 3Cvogais.py to vogais.py

--- vogais.py
+++ vogais.py
@@ -1,5 +1,5 @@
-n = 'a'
-var = False
+# Função que recebe um parâmetro e devolve True caso o parâmetro seja uma vogal ou False, caso contrário
+
 def vogal (n):
     if n== 'a'or n=='A'or n=='e'or n=='E'or n=='I'or n=='i'or n=='O'or n=='o'or n=='u'or n=='U':
         var = True
